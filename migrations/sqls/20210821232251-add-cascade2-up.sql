b8e10de4020adf9a1f76bf390f6e90b239dd4e98
jgrindall <john.grindall@gmail.com>
2021-08-22 08:57:48 +0100

    reset file

--- migrations/sqls/20210821232251-add-cascade2-up.sql
+++ migrations/sqls/20210821232251-add-cascade2-up.sql
@@ -1,8 +1,27 @@
 alter table order_products
-    drop constraint order_products_order_id_fkey;
+    drop constraint order_products_order_id;
 
 alter table order_products
-    drop constraint order_products_product_id_fkey;
+    drop constraint order_products_product_id;
 
 alter table orders
-    drop constraint orders_user_id_fkey;
+    drop constraint orders_user_id;
+
+
+
+alter table order_products
+    add constraint order_products_order_id
+        foreign key (order_id)
+            references orders(id) on delete cascade;
+
+alter table order_products
+    add constraint order_products_product_id
+        foreign key (product_id)
+            references products(id) on delete cascade;
+
+
+alter table orders
+    add constraint orders_user_id
+        foreign key (user_id)
+            references users(id) on delete cascade;
+
